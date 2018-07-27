056eaa1cf3edb80f0c2fa5dd3a9ed4f0a198841c
Fn <35122638+ChaiJiangBo@users.noreply.github.com>
2018-07-27 17:18:23 +0800

    https://blog.csdn.net/lw_power/article/details/80876641

--- BitwiseOfNumberRange.java
+++ BitwiseOfNumberRange.java
@@ -9,6 +9,7 @@ package leetcode.test.chaijb;
  *
  */
 public class BitwiseOfNumberRange {
+	//看了别人的答案，不得不承认，自己的解法很丑，不能普适，现增加两个方法
 	public int rangeBitwiseAnd(int m, int n) {
 		int max;
 		if (n == Integer.MAX_VALUE) {
@@ -30,6 +31,31 @@ public class BitwiseOfNumberRange {
 			result = result & result;
 		}
 		return result;
+	}
+	/*
+	我们发现，结果只与这些数中最左边“最长”的相等的部分有关，因此，我们可以将 m 和 n 不断右移
+	（题目中说了 m 和 n 不是负数，所以不存在右移符号位的问题），移到相等的时候为止。
+	同时记录移动的步数。最后将相等的部分左移之前的步数（相当于补 0），就是最终要求的数。
+
+	思路2：位运算做得题多了，我们就会知道与运算的一条性质：n & (n - 1) 可以将 n 最右边的 1 变成 0 ，
+	这件事情也是很酷的，因为其实不仅把 1 变成了 0，还跳过了很多 0，
+	所以我们从暴力解法的反方向去思考，倒着做按位与，就可以很快得到解了。
+	*/
+	
+	public int rangeBitwiseAnd2(int m, int n) {
+		while (n > m) {
+            n &= (n - 1);
+        }
+        return n;
+	}
+	public int rangeBitwiseAnd3(int m, int n) {
+		int count = 0;
+		while(m != n) {
+			m = m >> 1;
+			n = n >> 1;
+			count ++;
+		}
+		return (m << count);
 	}
 
 	public static void main(String[] args) {
