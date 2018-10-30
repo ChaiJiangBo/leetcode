f76077ea3beae98021c9c79f7dc9c2d6d2e20d17
Fn <35122638+ChaiJiangBo@users.noreply.github.com>
2018-10-30 17:09:40 +0800

    https://leetcode-cn.com/problems/counting-bits/description/
    @author chaijb
     * 338. 比特位计数
     * 给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。

---
+++ CountBits.java
@@ -0,0 +1,18 @@
+package leetcode.test.chaijb;
+
+/**
+ * @author chaijb
+ * 338. 比特位计数
+ * 给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
+ * https://leetcode-cn.com/problems/counting-bits/description/
+ *
+ */
+public class CountBits {
+    public int[] countBits(int num) {
+        int[] result = new int[num+1];
+        for (int i = 1; i <= num; i++) {
+            result[i] = result[i & (i-1)] + 1;
+        }
+        return result;
+    }
+}
